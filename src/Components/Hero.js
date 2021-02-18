76efdb874902fb66805b30d1fd5285f2926bbe5a
josue <jovz1798@gmail.com>
2021-02-18 16:23:15 -0600

    hero swiper working with real data

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -13,35 +13,65 @@ SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 export const Hero = () => {
 
-    const images = [10,30,70,100,1000];
     const [movies, setMovies] = useState([]);
+    const [imgPerView, setImgPerView] = useState(1);
+
     useEffect(() => {
         
         getPopularMovies().then( data => {
             
             setMovies( data );
         })
+
+
+        // Set movies per view when initializing swiper
+        if(window.innerWidth >= 1000){
+            setImgPerView(3);
+        }
+        if(window.innerWidth >= 500 && window.innerWidth <= 999 ){
+            setImgPerView(2);
+        }
+        // if no enter one of the above options, sets the swiper to only one movie per view
+
+        // Event for controlling swiper responsiveness
+        window.addEventListener('resize', () =>{
+
+            if( window.innerWidth < 500){
+                setImgPerView(1);
+            }
+            if( window.innerWidth >= 500){
+                setImgPerView(2);
+            }
+            if( window.innerWidth >= 1000){
+                setImgPerView(3);
+            }
+
+        });
+
+        return () => {
+            window.removeEventListener('resize',window);
+        }
+
+
     }, [])
 
     return (
-        <div>
+        <div className="hero">
+
+            <h1 className="hero__title"><span className="hero__title--big">Welcome</span>, <br /> Explore your favorite movies, Tv shows and celebrities</h1>
             
             <Swiper 
                 autoplay={ {delay: 2000} }
                 // preloadImages
                 navigation
+                slidesPerView={imgPerView}
                 pagination={ {clickable: true} }>
-                {/* {
-                    movies.map( movie => (
-                        <SwiperSlide key={ movie.id }> 
-                            <img src={} alt={`slide-${img}`}/>
-                        </SwiperSlide>
-                    ))
-                } */}
+
                 {
-                    images.map( img => (
-                        <SwiperSlide key={`image-${img}`}> 
-                            <img src={`https://picsum.photos/id/${img}/200/300`} alt={`slide-${img}`}/>
+                    movies.map( movie => (
+                        <SwiperSlide key={ movie.id } onClick={ ()=> { console.log(`click on ${movie.id}`)}}> 
+                        {/* create card for every movie */}
+                            <img src={movie.backdrop_path} alt={movie.title}/>
                         </SwiperSlide>
                     ))
                 }
--- src/helpers/theMovieDB.js
+++ src/helpers/theMovieDB.js
@@ -1,7 +1,7 @@
 
 const baseUrl = 'https://api.themoviedb.org/3/';
 const apiKey= 'd8532795110a063abc81c58c1a79d440';
-
+const imgBaseUrl = 'https://image.tmdb.org/t/p/w500'
 export const getPopularMovies = async() => {
     
     try {
@@ -12,6 +12,16 @@ export const getPopularMovies = async() => {
             const popularResponse = await resp.json();
             const { results } = popularResponse;
             
+             results.map( movie => {
+                if( movie.backdrop_path){
+                    movie.backdrop_path = `${ imgBaseUrl}${movie.backdrop_path}`;
+                }
+                if( movie.poster_path){
+                    movie.poster_path = `${ imgBaseUrl}${movie.poster_path}`;
+                }
+                return movie
+            });
+
             return results;
         } 
         
