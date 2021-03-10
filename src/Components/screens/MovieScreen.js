14ee91ea1ccfd7731e3714c3c5347f89221cee94
josue <jovz1798@gmail.com>
2021-03-10 17:31:35 -0600

    starting details screen markup and styling

--- src/Components/screens/MovieScreen.js
+++ src/Components/screens/MovieScreen.js
@@ -1,30 +1,52 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import { cleanUpSelectedMovie, startfetchMovieDetails } from '../../actions/movies';
 
 export const MovieScreen = () => {
 
    const { id } = useParams();
+   const { goBack } = useHistory();
    const { selectedMovie } = useSelector(state => state.movies)
+   
    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch( startfetchMovieDetails( id ) );
-        return () => {
-            dispatch( cleanUpSelectedMovie() );
+   
+   
+   useEffect(() => {
+       dispatch( startfetchMovieDetails( id ) );
+       return () => {
+           dispatch( cleanUpSelectedMovie() );
         }
-    }, [id, dispatch ])
+    }, [id, dispatch ])    
 
     return (
         <div className="screen">
-            <h1>Movie Screen</h1>
+            {
+                (selectedMovie) &&
 
-            <code>
-                {
-                JSON.stringify( selectedMovie )
-                }
-            </code>
+                <div className="detail-screen">
+                    <h1>{ selectedMovie.title }</h1>
+                    <span>"{ selectedMovie.tagline }"</span>
+                    <figure className="detail-screen__poster">
+                        <img src={ selectedMovie.poster_path } alt={ selectedMovie.title }/>
+                    </figure>
+                    <button className="detail-screen__btn" onClick={ goBack }>Go back</button>
+                    <p className="detail-screen__description">{ selectedMovie.overview }</p>
+                    {
+                        (selectedMovie.homepage) &&
+                        <a className="detail-screen__call-to-action" href={selectedMovie.homepage} target="_blank" rel="noreferrer">Home Page</a>
+                    }
+                    {
+                        selectedMovie.genres.map( genre => (
+                            <span key={ genre.id } className="detail-screen__genres">{ genre.name}</span>
+                        )
+                        )
+                    }
+
+                </div>
+            }
+
+           
 
         </div>
     )
